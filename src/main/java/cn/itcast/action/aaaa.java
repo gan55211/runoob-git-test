c5235419aa96998d2788255db48e994ff2fb1adc
ganfeng <18871111636@163.com>
2017-09-15 13:27:36 +0800

    aaa

---
+++ src/main/java/cn/itcast/action/aaaa.java
@@ -0,0 +1,9 @@
+package cn.itcast.action;
+
+public class aaaa {
+	public static void main(String[] args) {
+		System.out.println("aaaaa");
+		/*RuntimeException exception=null;
+		throw exception;*/
+	}
+}
